d52e018903499125a96cf789c1b1ec72473b719e
Shashank Mishra <shkcodes@gmail.com>
2021-05-24 01:40:44 +0530

    remove getters for dimens

--- app/src/main/java/com/shkcodes/aurora/theme/Dimens.kt
+++ app/src/main/java/com/shkcodes/aurora/theme/Dimens.kt
@@ -1,38 +1,35 @@
 package com.shkcodes.aurora.theme
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 object Dimens {
-    val two_dp: Dp @Composable get() = 2.dp
-    val space_small: Dp @Composable get() = 4.dp
-    val space: Dp @Composable get() = 8.dp
-    val space_large: Dp @Composable get() = 24.dp
-    val space_xxxlarge: Dp @Composable get() = 56.dp
+    val two_dp = 2.dp
+    val space_small = 4.dp
+    val space = 8.dp
+    val space_large = 24.dp
+    val space_xxxlarge = 56.dp
 
-    val keyline_1: Dp @Composable get() = 16.dp
+    val keyline_1 = 16.dp
 
-    val text_body: TextUnit @Composable get() = 16.sp
-    val text_caption: TextUnit @Composable get() = 12.sp
-    val text_large: TextUnit @Composable get() = 18.sp
+    val text_body = 16.sp
+    val text_caption = 12.sp
+    val text_large = 18.sp
 
-    val corner_radius: Dp @Composable get() = 4.dp
+    val corner_radius = 4.dp
 
     // Timeline
-    val tweet_profile_pic: Dp @Composable get() = 32.dp
-    val single_row_media_height: Dp @Composable get() = 170.dp
-    val multi_row_media_height: Dp @Composable get() = 100.dp
-    val link_preview_height: Dp @Composable get() = 75.dp
-    val gif_indicator_size: Dp @Composable get() = 28.dp
-    val play_icon_size: Dp @Composable get() = 36.dp
-    val video_player_max_height: Dp @Composable get() = 200.dp
-    val default_corner_radius: Dp @Composable get() = 2.dp
-    val bottom_nav_indicator_height: Dp @Composable get() = 0.2.dp
+    val tweet_profile_pic = 32.dp
+    val single_row_media_height = 170.dp
+    val multi_row_media_height = 100.dp
+    val link_preview_height = 75.dp
+    val gif_indicator_size = 28.dp
+    val play_icon_size = 36.dp
+    val video_player_max_height = 200.dp
+    val default_corner_radius = 2.dp
+    val bottom_nav_indicator_height = 0.2.dp
 
     // Profile
-    val profile_pic_size: Dp @Composable get() = 100.dp
-    val banner_height: Dp @Composable get() = 180.dp
+    val profile_pic_size = 100.dp
+    val banner_height = 180.dp
 }
