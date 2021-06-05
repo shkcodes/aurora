a725c8f691665cf1a343efd983ee792f12d5af5c
Shashank Mishra <shkcodes@gmail.com>
2021-06-05 16:04:41 +0530

    add media viewer tests

---
+++ app/src/test/java/com/shkcodes/aurora/media/MediaViewerViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.shkcodes.aurora.media
+
+import com.shkcodes.aurora.base.BaseTest
+import com.shkcodes.aurora.cache.entities.MediaEntity
+import com.shkcodes.aurora.service.UserService
+import com.shkcodes.aurora.ui.media.MediaViewerContract.Intent.Init
+import com.shkcodes.aurora.ui.media.MediaViewerContract.State
+import com.shkcodes.aurora.ui.media.MediaViewerViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+class MediaViewerViewModelTest : BaseTest() {
+
+    private val media = mockk<MediaEntity>()
+
+    private val userService = mockk<UserService> {
+        coEvery { getMediaForTweet(any()) } returns listOf(media)
+    }
+
+    private fun viewModel() = MediaViewerViewModel(testDispatcherProvider, userService)
+
+    @Test
+    fun `updates state correctly on init`() = testDispatcher.runBlockingTest {
+        val sut = viewModel()
+        sut.handleIntent(Init(40, 600))
+
+        sut.testStates {
+            assert(State() == expectItem())
+            assert(State(40, listOf(media)) == expectItem())
+        }
+    }
+}
